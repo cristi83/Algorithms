cfb61bd5bf8f021ce7948e20aba592fc7bdba0c4
cristi83 <cristian.alexandru1983@gmail.com>
2017-08-08 23:37:23 +0300

    Second commit

--- src/Algorithms.java
+++ src/Algorithms.java
@@ -1,6 +1,7 @@
 
 public class Algorithms {
-    public static void main (String args[]){
+    public static void main (String args[]) {
+        System.out.print("ExampleGit");
         System.out.print("ExampleGit");
     }
 }
